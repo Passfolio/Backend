7c6ffab313218be0516924d757e2db84218dedda
Hooby <hooby11105@naver.com>
2026-04-05 14:08:31 +0800

    feat: Define OAuth2UserDto

---
+++ src/main/java/com/capstone/passfolio/domain/auth/oauth2/dto/OAuth2UserDto.java
@@ -0,0 +1,65 @@
+package com.capstone.passfolio.domain.auth.oauth2.dto;
+
+import com.capstone.passfolio.common.util.HmacUtil;
+import com.capstone.passfolio.domain.user.entity.User;
+import com.capstone.passfolio.domain.user.entity.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder @NoArgsConstructor @AllArgsConstructor @Data
+public class OAuth2UserDto {
+    private Long userId;
+    private Role role;
+    private String nickname;
+    private String username;
+    private String email;
+    private String profileImageUrl;
+    private boolean isEmailVerified;
+
+    public static OAuth2UserDto of(Role role, OAuth2Response oAuth2Response, HmacUtil hmacUtil) {
+        return OAuth2UserDto.builder()
+                .userId(null) // OAuth2Response 엔 userId가 없음 -> user 등록 하고 해당 DTO update 해야 함.
+                .role(role)
+                .nickname(suggestNickname(oAuth2Response, hmacUtil))
+                .username(generateUsername(oAuth2Response, hmacUtil))
+                .profileImageUrl(oAuth2Response.getProfileImageUrl())
+                .build();
+    }
+
+    public static OAuth2UserDto from(User user) {
+        return OAuth2UserDto.builder()
+                .userId(user.getId()) // user 등록된 이후 DTO Update 됨
+                .role(user.getRole())
+                .nickname(user.getNickname())
+                .username(user.getUsername())
+                .profileImageUrl(user.getProfileImageUrl())
+                .build();
+    }
+
+    public User toUser() {
+        return User.builder()
+                .role(this.role)
+                .nickname(this.nickname)
+                .username(this.username)
+                .profileImageUrl(this.profileImageUrl)
+                .build();
+    }
+
+    private static String generateUsername(OAuth2Response oAuth2Response, HmacUtil hmacUtil) {
+        String hashedId = hmacUtil.hmacSha256Base64(oAuth2Response.getProviderId());
+        return oAuth2Response.getProvider() + "_" + hashedId;
+    }
+
+    private static String suggestNickname(OAuth2Response oAuth2Response, HmacUtil hmacUtil) {
+        // 닉네임 null/blank 시 대체.
+        String nickname = oAuth2Response.getNickname();
+
+        if (!nickname.isEmpty()) return nickname;
+
+        String hashedId = hmacUtil.hmacSha256Base64(oAuth2Response.getProviderId());
+
+        return "user_" + oAuth2Response.getProvider() + "_" + hashedId;
+    }
+}
