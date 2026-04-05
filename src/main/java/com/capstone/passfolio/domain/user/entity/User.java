fe452ff51afa795cb7569263be00a4d147b22233
Hooby <hooby11105@naver.com>
2026-04-05 18:05:36 +0800

    fix: User->remove not used method (create method)

--- src/main/java/com/capstone/passfolio/domain/user/entity/User.java
+++ src/main/java/com/capstone/passfolio/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.capstone.passfolio.domain.user.entity;
 
+import com.capstone.passfolio.domain.auth.oauth2.entity.enums.ProviderType;
 import com.capstone.passfolio.domain.user.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -34,6 +35,13 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Builder.Default
     private Role role = Role.USER;
+
+    @Column(name = "provider_type", nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private ProviderType providerType;
+
+    @Column(name = "provider_id", nullable = false)
+    private String providerId; // Hashed Data Using HmacUtils
 
     @PrePersist // INSERT 되기 전 실행 (새로운 User 저장 시)
     @PreUpdate  // UPDATE 되기 전 실행 (기존 User 수정 시)
