726deb2571aa21f3fc723ecda9154bce8fbc2c30
Hooby <hooby11105@naver.com>
2026-04-05 14:06:27 +0800

    feat: HmacUtil for Security

---
+++ src/main/java/com/capstone/passfolio/common/util/HmacUtil.java
@@ -0,0 +1,33 @@
+package com.capstone.passfolio.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
+@Component
+public class HmacUtil {
+    private static final String HMAC_ALGO = "HmacSHA256";
+
+    @Value("${hmac.secret}")
+    String secretKey;
+
+    public String hmacSha256Base64(String input) {
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), HMAC_ALGO);
+            Mac mac = Mac.getInstance(HMAC_ALGO);
+            mac.init(keySpec);
+            byte[] raw = mac.doFinal(input.getBytes(StandardCharsets.UTF_8));
+
+            return Base64.getUrlEncoder().withoutPadding().encodeToString(raw);
+        } catch (Exception e) {
+            throw new RuntimeException("HMAC 실패", e);
+        }
+    }
+}
