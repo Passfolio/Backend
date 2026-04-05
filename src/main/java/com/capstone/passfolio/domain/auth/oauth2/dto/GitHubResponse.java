1272402eed6588f5a05ffda70851dd0d52bfba2d
Hooby <hooby11105@naver.com>
2026-04-05 14:23:10 +0800

    feat: Define GitHuyResponse

---
+++ src/main/java/com/capstone/passfolio/domain/auth/oauth2/dto/GitHubResponse.java
@@ -0,0 +1,30 @@
+package com.capstone.passfolio.domain.auth.oauth2.dto;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class GitHubResponse implements OAuth2Response {
+    private final Map<String, Object> attributes;
+
+    @Override public String getProvider() { return "github"; }
+
+    @Override public String getProviderId() {
+        Object id = attributes.get("id");
+        return (id == null) ? null : String.valueOf(id);
+    }
+
+    @Override public String getNickname() {
+        Object name = attributes.get("name");
+        if (name != null && !name.toString().isBlank()) return name.toString();
+        Object login = attributes.get("login");
+        return (login == null) ? null : login.toString();
+    }
+
+    @Override public String getProfileImageUrl() {
+        Object avatarUrl = attributes.get("avatar_url");
+        return (avatarUrl == null) ? null : avatarUrl.toString();
+    }
+}
+
