6911d8007defe0e6881944cba1d086b1cf60d637
Hooby <hooby11105@naver.com>
2026-04-05 13:51:45 +0800

    init configuration

---
+++ src/main/java/com/capstone/passfolio/PassfolioApplication.java
@@ -0,0 +1,12 @@
+package com.capstone.passfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassfolioApplication.class, args);
+    }
+}
---
+++ src/test/java/com/capstone/passfolio/PassfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.passfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
