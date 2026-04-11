e1387b7e4a7d09191eb7884ee29cce21bf0d31a1
Hooby <hooby11105@naver.com>
2026-04-11 21:23:01 +0900

    feat: Define DevSpec Repositories

---
+++ src/main/java/com/capstone/passfolio/domain/spec/repository/UniversityDepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.passfolio.domain.spec.repository;
+
+import com.capstone.passfolio.domain.spec.entity.UniversityDepartment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversityDepartmentRepository extends JpaRepository<UniversityDepartment, Long> {
+}
